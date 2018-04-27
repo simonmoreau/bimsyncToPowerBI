5f61a35d9bb008438d6054330f1f59f5858138d4
MOREAU <s.moreau@bouygues-immobilier.com>
2018-04-27 16:22:54 +0200

    Add document extension

--- Management/Tables/Documents.m
+++ Management/Tables/Documents.m
@@ -1,13 +1,25 @@
 let
+    //Get extension
+    GetExtension = (name as text) as text =>
+let
+    lenght = Text.Length(name),
+    position = Text.PositionOf(name, ".", Occurrence.Last),
+    extension = if  (lenght - position = 4 or lenght - position = 5)  and position <> -1 then
+                    Text.Lower(Text.Range(name,position,lenght-position))
+                else
+                    "folder"
+in
+    extension,
 
 	//Get a list of documents for a given projectId and libraryId
 	GetDocuments = (projectId as text, libraryId as text, token as text) as list =>
 let 
 	documents = RESTWPages("/v2/projects/" & projectId & "/libraries/" & libraryId & "/items",token),
 	documentsWithProjectId = List.Transform(documents, each Record.AddField(_,"projectId",projectId)),
-    documentsWithLibraryId = List.Transform(documents, each Record.AddField(_,"libraryId",libraryId))
+    documentsWithLibraryId = List.Transform(documentsWithProjectId, each Record.AddField(_,"libraryId",libraryId)),
+    documentsWithExtension = List.Transform(documentsWithLibraryId, each Record.AddField(_,"Extension",GetExtension(_[name])))
 in
-	documentsWithLibraryId,
+	documentsWithExtension,
 
     //The main function starts here
     token = GetToken(),
